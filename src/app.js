f5a77e72a519ff6e230e75054dcd612c9b9ab5c8
MontielCreate <luxorfer12@gmail.com>
2025-05-15 03:17:50 +0000

    generado fm

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ const pro = ["the","our"];
+ let randomIndexPro = Math.floor(Math.random()*pro.length)
+ let proRandom = pro[randomIndexPro]
+
+
+ const adj = ["great","big"];
+ let randomIndexAdj = Math.floor(Math.random()*adj.length)
+ let adjRandom = adj[randomIndexAdj]
+
+
+ const noun = ["jogger","racoon"];
+ let randomIndexNoun = Math.floor(Math.random()*noun.length)
+ let nounRandom = noun[randomIndexNoun]
+
+
+
+console.log(proRandom, adjRandom, nounRandom)
+
+ const ul = document.getElementById("domainList");
+  const li = document.createElement("li");
+  li.innerText = `${proRandom}${adjRandom}${nounRandom}.com`;
+  ul.appendChild(li);
+
 };
